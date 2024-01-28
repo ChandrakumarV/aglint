9b4ac7cd7a774f92f0b9647c3fc690f5571281d0
ChandrakumarV <chandrakumar252004@gmail.com>
2024-01-28 14:07:16 +0530

    url

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -60,7 +60,7 @@ export async function signup(signupFormData){
             data: {
               full_name: name,
             },
-            emailRedirectTo: 'http://localhost:5173/aglint/optionpick'
+            emailRedirectTo: 'https://chandrakumarv.github.io/aglint/optionpick'
           }
         }
     )
@@ -79,7 +79,7 @@ export async function forgotPassword({email}){
 
 const { data, error } = await supabase.auth.api.resetPasswordForEmail(
   email,
-  { redirectTo: 'http://localhost:5173/aglint/updatepassword' }
+  { redirectTo: 'https://chandrakumarv.github.io/aglint/updatepassword' }
 )
 
 console.log(data,error)
